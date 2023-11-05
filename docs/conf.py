d969a0f5a15b69c49fce2e9bb116643cebb37292
JiaWeiTeh <teh_jiawei@yahoo.com>
2023-11-05 13:56:03 +0100

    Fixed commit issues with ReadTheDocs

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ intersphinx_disabled_domains = ['std']
 # templates_path = ['_templates']
 
 # -- Options for HTML output
-html_theme_path = ["."]
+html_theme_path = ["./source/"]
 html_theme = 'renku'
 
 # -- Options for EPUB output
