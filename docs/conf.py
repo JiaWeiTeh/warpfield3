5294bfbfc4710a8aa41ae2bf6c7af0df39ba2636
JiaWeiTeh <teh_jiawei@yahoo.com>
2023-11-05 14:42:06 +0100

    Fixed commit issues with ReadTheDocs

--- docs/conf.py
+++ docs/conf.py
@@ -9,6 +9,23 @@ author = 'Jia Wei Teh'
 release = '0.1'
 version = '0.1.0'
 
+import os
+import sys
+
+# Mock imports to avoid problems on readthedocs
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    from mock import Mock as MagicMock
+    class Mock(MagicMock):
+        @classmethod
+        def __getattr__(cls, name):
+            return Mock()
+    MOCK_MODULES = ['numpy', 'scipy', 'astropy', 'scipy.interpolate', 
+                    'scipy.constants', 'astropy.io', 'astropy.io.fits',
+                    'astropy.io.ascii', 'numpy.ctypeslib']
+    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+    
+    
 # -- General configuration
 
 extensions = [
@@ -17,19 +34,20 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    # 'sphinx_rtd_theme'
+    'sphinx_rtd_theme'
 ]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
+
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
 # -- Options for HTML output
-html_theme_path = ["_theme"]
+# html_theme_path = ["_theme"]
 html_theme = 'sphinx-rtd-theme'
 
 # -- Options for EPUB output
